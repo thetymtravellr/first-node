788bc24bb58c4143fa83ff557dc6b62c79731f9d
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-04-20 16:23:20 +0600

    steps

---
+++ _steps.js
@@ -0,0 +1,26 @@
+/* 
+1. create a folder
+2. open command line to that folder
+3. npm init
+
+*------------------------
+for express
+*-----------------------
+1. npm install express
+2. require express
+3. declare port
+4. set app.get('/')
+5. listen to port
+6. node index.js
+7. check your browser for that port
+
+--------------
+get
+--------------
+1.cors
+--------------
+post
+--------------
+1.
+
+*/
